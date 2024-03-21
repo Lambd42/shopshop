4fb901a4807bf67c3c0a53cecb0808152992171b
Lambda <tom.billet@ecoles-epsi.net>
2024-03-21 15:10:44 +0100

    ajout Invoice Model

--- src/Model/InvoiceModel.php
+++ src/Model/InvoiceModel.php
@@ -16,23 +16,23 @@ class InvoiceModel {
         $this->db = $db;
     }
 
-    public function getAllOrders():array {
+    public function getAllInvoices():array {
         $sql = "SELECT Invoice.invoiceID, invoiceDate, totalAmount, PaymentStatus, Order.orderID, orderDate, status, User.userID, firstName, lastName, email, address, postalCode, city, phone, password, roles FROM Invoice INNER JOIN Order ON Invoice.orderID = Order.orderID INNER JOIN User ON Order.userID = User.userID;";
         $stmt = $this->db->query($sql);
         $invoices = [];
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $user = new User($row['User.userID'],$row['email'],$row['firstName'],$row['lastName'],$row['password'],json_decode($row['roles']));
-            $invoice = new Invoice($row['Invoice.invoiceID'], $row[invoiceDate], $row[totalAmount], $row[paymentStatus], $row[Order.orderID]);
-            $orders[] = new Order($row[Order.orderID], $row[orderDate], $row[status], $user, $invoice);
+            $order = new Order($row[Order.orderID], $row[orderDate], $row[status], $user);
+            $invoices[] = new Invoice($row['Invoice.invoiceID'], $row[invoiceDate], $row[totalAmount], $row[paymentStatus], $order);
         }
 
-        return $orders;
+        return $invoices;
 
     }
 
-    public function getOneOrder($id): ?Order {
-        $sql = "SELECT Order.orderID, orderDate, status, User.userID, firstName, lastName, email, address, postalCode, city, phone, password, roles, Invoice.invoiceID, invoiceDate, totalAmount, paymentStatus FROM Order INNER JOIN User ON Order.userID = User.userID INNER JOIN Invoice ON Order.invoiceID = Invoice.invoiceID WHERE Order.orderID = :id;";
+    public function getOneInvoice($id): ?Order {
+        $sql = "SELECT Invoice.invoiceID, invoiceDate, totalAmount, PaymentStatus, Order.orderID, orderDate, status, User.userID, firstName, lastName, email, address, postalCode, city, phone, password, roles FROM Invoice INNER JOIN Order ON Invoice.orderID = Order.orderID INNER JOIN User ON Order.userID = User.userID WHERE Invoice.invoiceID = :id;";
         $stmt = $this->db->prepare($sql);
         $stmt->bindValue(":id", $id);
         $stmt->execute();
@@ -41,26 +41,43 @@ class InvoiceModel {
             return null;
         }
         $user = new User($row['User.userID'],$row['email'],$row['firstName'],$row['lastName'],$row['password'],json_decode($row['roles']));
-        $invoice = new Invoice($row['Invoice.invoiceID'], $row[invoiceDate], $row[totalAmount], $row[paymentStatus], $row[Order.orderID]);
-        return new Order($row[Order.orderID], $row[orderDate], $row[status], $user, $invoice);
+        $order = new Order($row[Order.orderID], $row[orderDate], $row[status], $user);
+        return new Invoice($row['Invoice.invoiceID'], $row[invoiceDate], $row[totalAmount], $row[paymentStatus], $order);
 
     }
 
-    public function getOrdersByUser(User $user): array {
-        $sql = "SELECT Order.orderID, orderDate, status, User.userID, firstName, lastName, email, address, postalCode, city, phone, password, roles, Invoice.invoiceID, invoiceDate, totalAmount, paymentStatus FROM Order INNER JOIN User ON Order.userID = User.userID INNER JOIN Invoice ON Order.invoiceID = Invoice.invoiceID WHERE Order.userID = :userID;";
+    public function getInvoiceByUser(User $user): array {
+        $sql = "SELECT Invoice.invoiceID, invoiceDate, totalAmount, PaymentStatus, Order.orderID, orderDate, status, User.userID, firstName, lastName, email, address, postalCode, city, phone, password, roles FROM Invoice INNER JOIN Order ON Invoice.orderID = Order.orderID INNER JOIN User ON Order.userID = User.userID WHERE User.userID = :userId;";
         $stmt = $this->db->prepare($sql);
         $stmt->bindValue(":userID", $user.getUserId());
         $stmt->execute();
-        $orders = [];
+        $invoices = [];
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $user = new User($row['User.userID'],$row['email'],$row['firstName'],$row['lastName'],$row['password'],json_decode($row['roles']));
-            $invoice = new Invoice($row['Invoice.invoiceID'], $row[invoiceDate], $row[totalAmount], $row[paymentStatus], $row[Order.orderID]);
-            $orders[] = new Order($row[Order.orderID], $row[orderDate], $row[status], $user, $invoice);
+            $order = new Order($row[Order.orderID], $row[orderDate], $row[status], $user);
+            $invoices[] = new Invoice($row['Invoice.invoiceID'], $row[invoiceDate], $row[totalAmount], $row[paymentStatus], $order);
         }
 
         return $orders;
 
+
+    }
+
+    public function getInvoiceByOrder(Order $order) {
+        $sql = "SELECT Invoice.invoiceID, invoiceDate, totalAmount, PaymentStatus, Order.orderID, orderDate, status, User.userID, firstName, lastName, email, address, postalCode, city, phone, password, roles FROM Invoice INNER JOIN Order ON Invoice.orderID = Order.orderID INNER JOIN User ON Order.userID = User.userID WHERE Order.orderID = :orderId;";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(":orderId", $order.getId());
+        $stmt->execute();
+        $invoices = [];
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $user = new User($row['User.userID'],$row['email'],$row['firstName'],$row['lastName'],$row['password'],json_decode($row['roles']));
+            $order = new Order($row[Order.orderID], $row[orderDate], $row[status], $user);
+            $invoices[] = new Invoice($row['Invoice.invoiceID'], $row[invoiceDate], $row[totalAmount], $row[paymentStatus], $order);
+        }
+
+        return $orders;
 
     }
 }
